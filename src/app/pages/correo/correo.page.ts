3e41d42ecef6d4894ed95f8cf5562846759ad459
Danae Muñoz Opitz <dana.munozo@duocuc.cl>
2024-11-09 13:56:58 -0300

    aja

--- src/app/pages/correo/correo.page.ts
+++ src/app/pages/correo/correo.page.ts
@@ -1,63 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonCol, IonCardContent, IonLabel, IonItem, IonRow, IonButton, IonAlert } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { NavigationExtras, Router } from '@angular/router';
-import { Usuario } from 'src/app/model/usuario';
 import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/data-base.service';
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-correo',
   templateUrl: './correo.page.html',
   styleUrls: ['./correo.page.scss'],
   standalone: true,
-  imports: [IonAlert, IonButton, IonRow, IonItem, IonLabel, IonCardContent, IonCol, IonImg, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class CorreoPage implements OnInit {
 
-  public correo: string = '';
-  public isAlertOpen: boolean = false;
-  public alertButtons: any[] = [];
+  correo = '';
 
-  constructor(private router: Router, private alertController: AlertController) { }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  constructor(private router: Router, private bd: DataBaseService , private authService: AuthService) { }
+
+  ngOnInit() {
   }
 
-
-  
   async recuperarContrasena() {
-    // Verifica que this.correo esté definido y no vacío
-    if (!this.correo || this.correo.trim() === '') {
-      const alert = await this.alertController.create({
-        header: 'Aviso',
-        message: 'Campo obligatorio',
-        buttons: ['OK']
-      });
-  
-      await alert.present();
+    const usu=await this.bd.leerUsuario(this.correo);
+    if (usu == undefined){
+    this.router.navigate(['/incorrecto']);
+  }else{
+    this.router.navigate(['/pregunta']);
+    if (!usu) {
+      this.router.navigate(['/incorrecto']);
     } else {
-      // Busca el usuario por correo
-      const usuarioEncontrado = Usuario.buscarUsuarioPorCorreo(this.correo);
-  
-      if (!usuarioEncontrado) {
-        // Redirige a la página de error si no se encuentra el usuario
-        this.router.navigate(['/incorrecto']);
-      } else {
-        // Si el usuario es encontrado, redirige a la página de pregunta secreta
-        const navigationExtras: NavigationExtras = {
-          state: {
-            usuario: usuarioEncontrado
-          }
-        };
-        this.router.navigate(['/pregunta'], navigationExtras);
-      }
+      const navigationExtras: NavigationExtras = {
+        state: {
+          usuario: usu
+        }
+      };
+      this.router.navigate(['/pregunta'], navigationExtras);
     }
   }
   
+  }
 
   volverAlInicio() {
     this.router.navigate(['/login']);
   }
 
 }
+
